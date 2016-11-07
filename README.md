7c54ca00c306282c9080d91c45f520d5384fe1a1
Patrick Beckedorf <symfonyit@gmail.com>
2016-11-07 01:55:43 +0100

    implemented exception handling for api

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-webrtc
+webrtc api backend.
+>> This includes signaling server and example application.
+
+>> REST Api is partly inspired by http://knpuniversity.com/tracks/rest
+
+>> Framework used for API: Symfony 3.x
+
+>> Current developers:
+Patrick (novacp), Markus, Hadi.
--- src/AppBundle/Controller/Controller.php
+++ src/AppBundle/Controller/Controller.php
@@ -8,6 +8,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Api\ApiProblem;
+use AppBundle\Api\ApiProblemException;
 use JMS\Serializer\SerializationContext;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller as BaseController;
 use Symfony\Component\Form\FormInterface;
@@ -20,13 +22,54 @@ class Controller extends BaseController
     // POST (create)    : 201
     // Delete (delete)  : 204
     // PUT (update)     : 200
+    // Validation errors: 400
+
+    // Form errors, put before dump: header('Content-type: cli');
 
     // -> put + patch in one method, but within process form, change argument to:
-    private function processForm(Request $request, FormInterface $form)
+    protected function processForm(Request $request, FormInterface $form)
     {
         $data = json_decode($request->getContent(), true);
+        if(null === $data)
+        {
+            $apiProblem = new ApiProblem(400, ApiProblem::TYPE_INVALID_REQUEST_BODY_FORMAT);
+            throw new ApiProblemException($apiProblem);
+        }
         $clearMissing = $request->getMethod() != 'PATCH';
         $form->submit($data, $clearMissing);
+    }
+
+    // Error content type: application/problem+json
+    // on an error, add the following json structure:
+    // type => internal error name, e.g. 'validation_error'
+    // title => human readable error, e.g. 'There was an error'
+    // errors => errors
+    protected function getErrorsFromForm(FormInterface $form)
+    {
+        $errors = array();
+        foreach ($form->getErrors() as $error) {
+            $errors[] = $error->getMessage();
+        }
+        foreach ($form->all() as $childForm) {
+            if ($childForm instanceof FormInterface) {
+                if ($childErrors = $this->getErrorsFromForm($childForm)) {
+                    $errors[$childForm->getName()] = $childErrors;
+                }
+            }
+        }
+        return $errors;
+    }
+
+
+    // CSRF tokens __not__ required, as stateless api.  No application with a session (no cookies on client side, and no session on server side)
+    protected function throwApiProblemValidationException(FormInterface $form)
+    {
+        $errors = $this->getErrorsFromForm($form);
+
+        $apiProblem = new ApiProblem(400, ApiProblem::TYPE_VALIDATION_ERROR);
+        $apiProblem->set('errors', $errors);
+
+        throw new ApiProblemException($apiProblem);
     }
 
     protected function createApiResponse($data, $statusCode = 200)
